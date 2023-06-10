f095f6ad8fb4a38159852fe870faa234aaf50b8a
HASSAN ED-DYB <107430322+hassandyb@users.noreply.github.com>
2023-06-10 16:41:51 +0100

    Add files via upload

--- exam_rank_3/get_next_line.c
+++ exam_rank_3/get_next_line.c
@@ -6,6 +6,14 @@
 
 #define BUFFER_SIZE 45
 
+int ft_strlen(char *str)
+{
+	int i = 0;
+	while(str && str[i])
+		i++;
+	return i;
+}
+
 int ft_check(char *stock)
 {
 	int i = 0;
@@ -25,15 +33,103 @@ char *ft_strjoin(char *stock, char *buffer)
 	{
 		stock = malloc(sizeof(char));
 		if(stock == NULL)
-		{
-			free(buffer);
 			return NULL;
-		}
 		stock [0] = '\0';
 	}
-	char *temp = malloc
+	char *temp = malloc((ft_strlen(stock) + ft_strlen(buffer) + 1) * sizeof(char));
+	if(temp == NULL)
+		return NULL;
+	int i = 0;
+	while(stock[i])
+	{
+		temp[i] = stock[i];
+		i++;
+	}
+	int j = 0;
+	while(buffer[j])
+	{
+		temp[i] = buffer[j];
+		i++;
+		j++;
+	}
+	temp[i] = '\0';
+	free (stock);
+	return temp;
+	
 
 }
+
+int ft_linelen(char *stock)
+{
+	int i = 0;
+	while(stock[i])
+	{
+		if(stock[i] == '\n');
+		{
+			i++;
+			return i;
+		}
+		i++;
+	}
+	return i;
+}
+
+char *ft_get_befor_newline(char *stock)
+{
+	if(stock == NULL || *stock == '\0')
+		return NULL;
+	int len = ft_linelen(stock);
+	char *line = malloc((len + 1) * sizeof(char));
+	int i = 0;
+	while(stock[i])
+	{
+		if(stock[i] == '\n')
+		{
+			line[i] = stock[i];
+			i++;
+			break;
+		}
+		line[i] = stock[i];
+		i++;
+	}
+	line[i] = '\0';
+	return line;
+}
+
+int ft_afterlinelen(char *stock)
+{
+	int i = 0;
+	while(stock[i])
+	{
+		if(stock[i] == '\n')
+			break;
+		i++;
+	}
+	i++;
+	int j = 0;
+	while(stock[i])
+	{
+		j++;
+		i++;
+	}
+	return j;
+}
+char *ft_get_after_newline(char *stock)
+{
+	if(stock == NULL || *stock == '\0')
+		return NULL;
+	if(ft_linelen(stock) == ft_strlen(stock))
+		return NULL;
+	int i = 0;
+	while(stock[i])
+	{
+		if(stock[i] == '\n')
+			break;
+		i++;
+	}
+	int len = ft_afterlinelen(stock);
+}
+
 char	*get_next_line(int fd)
 {
 	int r = 68345;
@@ -49,7 +145,12 @@ char	*get_next_line(int fd)
 		buffer[r] = '\0';
 		stock = ft_strjoin(stock, buffer);
 	}
+	free(buffer);
+	char line = ft_get_befor_newline(stock);
+	stock = ft_get_after_newline(stock);
 }
+
+
 
 
 
